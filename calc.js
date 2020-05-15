1f7c58188d06df7e36e325bae5e67ff0982ef6cb
Calvin Conley <cconley0125@gmail.com>
2020-05-15 14:00:37 -0400

    added display for numbers and clear functionality

--- calc.js
+++ calc.js
@@ -1,3 +1,4 @@
+//functions for performing operations
 function add(num1, num2) {
     return num1+num2;
 }
@@ -17,3 +18,21 @@ function divide(num1,num2) {
 function operate(num1, num2, operator) {
     return operator(num1, num2);
 }
+//gets display and numbers from html file
+//then saves number to variable and displays it
+const display = document.getElementById('display');
+const numbers = document.querySelectorAll('.number');
+let currentNumber = 0;
+function displayNumber(e) {
+    currentNumber = e.target.textContent;
+    display.textContent += `${currentNumber}`;
+}
+numbers.forEach(number => number.addEventListener('click', displayNumber))
+//adds functionality for clear button
+const clear = document.getElementById('clear');
+function clearNumbers() {
+    currentNumber = 0;
+    display.textContent = '';
+}
+clear.addEventListener('click', clearNumbers);
+//displays and stores operator
